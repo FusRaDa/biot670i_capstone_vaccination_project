63469c8f06198998e07660e0c80ae5559590a0f3
Matthew Rada <mrada@FusRaDa.mynetworksettings.com>
2026-08-24 10:16:23 -0400

    added proof of concept with toy data, start with outline next

--- README.md
+++ README.md
@@ -18,5 +18,23 @@
 - Plotly
 
 
+### Setup
+1. Clone repo to local computer
+    - git clone https://github.com/FusRaDa/biot670i_capstone_vaccination_project.git
+    - cd to biot670i_capstone_vaccination_project/
+
+2. Create python virtual environment (development - Python 3.13.9)
+    - ```python -m venv .venv```
+    - ```source .venv/bin/activate```
+    - these commands will slighty differ based on your system and method of installation
+
+3. Install dependancies - CHECK THAT YOU HAVE ACTIVATED VIRTUAL ENVIRONMENT
+    - ```pip install -r requirements.txt```
+
+4. Run toy data into streamlit
+    - ```streamlit run toy/app.py```
+
+
 ### Files & Directories
-toy/ - holds prrof of concept in terms of using the tech stack.
+- toy/ - holds prrof of concept in terms of using the tech stack.
+    - run ```streamlit run toy/app.py``` to see proof of concept
--- toy/app.py
+++ toy/app.py
@@ -1,10 +1,44 @@
 import streamlit as st
+import seaborn as sns
+import plotly.express as px
+import pandas as pd
+
 
 # Add a header title
-st.title("My First Streamlit App")
+st.title("BIOT 670I Group 1 - Toy Data")
+st.subheader("Restaurant Tips Analysis")
 
-# Add an interactive slider widget
-number = st.slider("Select a value", 0, 100, 50)
+# Data on restaurant tips
+df = sns.load_dataset('tips')
 
-# Display dynamic results text
-st.write(f"The square of {number} is {number ** 2}")
+
+# extract columns
+day = df['day']
+total_bill = df['total_bill']
+time = df['time']
+tip = df['tip']
+sex = df['sex']
+
+# Who tips more?
+day_tip_sex_gr = px.histogram(df, x=day, y=tip,
+             color=sex, barmode='group',
+             color_discrete_map={"Male": "blue", "Female": "red"},
+             title="Who gives the most tips?",
+             height=400)
+
+day_tip_sex_gr.update_yaxes(title_text="Tip Amount ($)")
+day_tip_sex_gr.update_xaxes(title_text="Day")
+
+
+# What day makes the most revenue?
+day_total_time_gr = px.histogram(df, x=day, y=total_bill,
+             color=time, barmode='group',
+             color_discrete_map={"Dinner": "blue", "Lunch": "yellow"},
+             title="What days make the most revenue?",
+             height=400)
+
+day_total_time_gr.update_yaxes(title_text="Total Bill ($)")
+day_total_time_gr.update_xaxes(title_text="Day")
+
+st.plotly_chart(day_tip_sex_gr)
+st.plotly_chart(day_total_time_gr)
