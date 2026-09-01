4adc05dc4853af63737de9e2051b1e73881c6175
Matthew Rada <mrada@FusRaDa.mynetworksettings.com>
2026-09-01 12:42:32 -0400

    extract more years from nis

--- raw/nis/download.bash
+++ raw/nis/download.bash
@@ -1,4 +1,104 @@
-mkdir -p raw/nis/{2015..2024}
+mkdir -p raw/nis/{1995..2024}
+
+# 1995
+wget -nc -P raw/nis/1995 \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf95dat.zip \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf95.sas
+
+# 1996
+wget -nc -P raw/nis/1996 \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf96dat.zip \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf96.sas
+
+# 1997
+wget -nc -P raw/nis/1997 \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf97dat.zip \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf97.sas
+
+# 1998
+wget -nc -P raw/nis/1998 \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf98.dat \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf98.sas
+
+# 1999
+wget -nc -P raw/nis/1999 \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/NIS/nispuf99dat.zip \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/NIS/nispuf99.sas
+
+# 2000
+wget -nc -P raw/nis/2000 \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf00dat.zip \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf00.sas
+
+# 2001
+wget -nc -P raw/nis/2001 \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf01dat.zip \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf01.sas
+
+# 2002
+wget -nc -P raw/nis/2002 \
+  https://ftp.cdc.gov/pub/health_statistics/nchs/Datasets/nis/nispuf02dat.zip \
+  https://ftp.cdc.gov/pub/health_statistics/nchs/Datasets/nis/nispuf02.sas
+
+# 2003
+wget -nc -P raw/nis/2003 \
+  https://ftp.cdc.gov/pub/health_statistics/nchs/Datasets/nis/nispuf03dat.zip \
+  https://ftp.cdc.gov/pub/health_statistics/nchs/Datasets/nis/nispuf03.sas
+
+# 2004
+wget -nc -P raw/nis/2004 \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf04dat.zip \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf04.sas
+
+# 2005
+wget -nc -P raw/nis/2005 \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf05_dat.zip \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf05.sas
+
+# 2006
+wget -nc -P raw/nis/2006 \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf06.dat.zip \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf06.sas
+
+# 2007
+wget -nc -P raw/nis/2007 \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf07.dat.zip \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf07.sas
+
+# 2008
+wget -nc -P raw/nis/2008 \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf08.dat \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf08.sas
+
+# 2009
+wget -nc -P raw/nis/2009 \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf09.dat \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf09.sas
+
+# 2010
+wget -nc -P raw/nis/2010 \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf10.dat \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf10.sas
+
+# 2011
+wget -nc -P raw/nis/2011 \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf11.dat \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf11.sas
+
+# 2012
+wget -nc -P raw/nis/2012 \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf12.dat \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf12.sas
+
+# 2013
+wget -nc -P raw/nis/2013 \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf13.dat \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf13.sas
+
+# 2014
+wget -nc -P raw/nis/2014 \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf14.dat \
+  https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/nis/nispuf14.sas
 
 # 2015
 wget -nc -P raw/nis/2015 \
@@ -49,3 +149,22 @@ wget -nc -P raw/nis/2023 \
 wget -nc -P raw/nis/2024 \
   https://www.cdc.gov/nis/media/files/2026/05/NISPUF24.DAT \
   https://www.cdc.gov/nis/media/files/2026/05/NISPUF24.SAS
+
+
+# extract zip and delete
+# unzip 1995 - 1997
+unzip -n raw/nis/1995/nispuf95dat.zip -d raw/nis/1995/
+unzip -n raw/nis/1996/nispuf96dat.zip -d raw/nis/1996/
+unzip -n raw/nis/1997/nispuf97dat.zip -d raw/nis/1997/
+
+
+# unzip 1999 - 2007
+unzip -n raw/nis/1999/nispuf99dat.zip -d raw/nis/1999/
+unzip -n raw/nis/2000/nispuf00dat.zip -d raw/nis/2000/
+unzip -n raw/nis/2001/nispuf01dat.zip -d raw/nis/2001/
+unzip -n raw/nis/2002/nispuf02dat.zip -d raw/nis/2002/
+unzip -n raw/nis/2003/nispuf03dat.zip -d raw/nis/2003/
+unzip -n raw/nis/2004/nispuf04dat.zip -d raw/nis/2004/
+unzip -n raw/nis/2005/nispuf05_dat.zip -d raw/nis/2005/
+unzip -n raw/nis/2006/nispuf06.dat.zip -d raw/nis/2006/
+unzip -n raw/nis/2007/nispuf07.dat.zip -d raw/nis/2007/
